eaa783b1461bbb008a395c0a45ca220420da76d5
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-10-12 22:11:34 +0000

    upload

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """ This is an empty class.
-        It shows the basic structure of a class."""
+    """ This is an empty class."""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Show the definition and instantiation of a private attribute"""
+    def __init__(self, size):
+        """The private attribute 'size' is defined here.
+
+            Args:
+                 size (int): This represents the size of the square and is
+                 passed from the module.
+        """
+
+        self.__size = size
