124d6033374a38cae7a0886b8984422eef26665d
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-10-13 19:17:17 +0000

    upload

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" This is a module with one class"""
+
+
+class Square:
+    """Show the definition and instantiation of a private attribute"""
+
+    def __init__(self, size=0):
+        """The private attribute 'size' is defined here.
+
+            Args:
+                 size (int): This represents the size of the square and is
+                 passed from the module.
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculates the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size  = value
