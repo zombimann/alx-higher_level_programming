80d151730664dc6e7ba722e854cccce93e6c8f1f
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-10-13 12:22:18 +0000

    upload

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,4 +10,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return result
     finally:
-        print("Inside result {}".format(result))
+        print("Inside result: {}".format(result))
