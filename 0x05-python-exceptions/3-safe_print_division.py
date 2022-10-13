9a3d86b24f545e505d473ac3e9529c736865a3a3
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-10-13 12:21:09 +0000

    upload

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result"""
+    result = None
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        return result
+    finally:
+        print("Inside result {}".format(result))
