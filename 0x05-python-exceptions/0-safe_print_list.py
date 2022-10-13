65ed1f8c03163b595176357100e89d4d8bf8d6a7
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-10-13 10:21:24 +0000

    upload

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 
 def safe_print_list(my_list=[], x=0):
     """ prints x elements of a list """
-    i = 0
+    i = -1
     try:
         for i in range(x):
             print(f'{my_list[i]}', end='')
